2fb51f2a5230d113fccc14bfbaa62c2a6b849c81
Rohan Mistry <rohanmistry231@gmail.com>
2024-10-30 16:44:03 +0530

    shifted backend api to Mongo Atlas Cloud

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors"); // Import CORS middleware
 const courseRoutes = require("./routes/courseRoutes");
 const noteRoutes = require("./routes/noteRoutes");
+require('dotenv').config();
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://127.0.0.1:27017/udemyCourses")
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Connection failed", err));
 
@@ -27,5 +28,5 @@ app.use("/courses", courseRoutes);
 app.use("/courses/:courseId/notes", noteRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
