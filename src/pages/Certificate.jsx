bceebf17ab128f4cbf2877a255d06635947a262c
Rohan Mistry <rohanmistry231@gmail.com>
2024-11-17 13:53:07 +0530

    Fix autofocus functionality on add Certifictate Modal

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -292,6 +292,7 @@ const Certificates = () => {
                     imageUrl: e.target.value,
                   })
                 }
+                autoFocus
               />
             </div>
 
